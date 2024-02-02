26f97cc15a8c359074610d0cfa856df2ebefd820
Tristan Simpson <heytristaann@gmail.com>
2024-02-01 20:27:11 -0500

    fix: InputField component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,97 +1,57 @@
-/**
- * Import React and the required types from React.
- */
-import React, {
-  ChangeEvent,
-  FC,
-  useState,
-  JSX,
-  // ReactNode,
-  InputHTMLAttributes,
-} from "react";
-
-/**
- * Import the 'cn' utility function to merge component classnames
- */
+import React, { FC, JSX, InputHTMLAttributes, useState } from "react";
 import { cn } from "@/lib/utilts/cn";
 
 /**
- * Default Props Values for the Input Field
+ * Ignored Input Props
  *
- * We use these in the case that the provided props are empty.
+ * These are the props that are ignored when passed to the input field.
  */
-const DEFAULT_PROPS = {
-  className: "",
-  defaultValue: "",
-  placeholder: "",
-};
+const IGNORED_INPUT_PROPS = ["className", "placeholder", "value", "onChange"];
 
 /**
- * Input Field Props
- *
- * These are used to define the props for the input field component.
+ * Custom Input Props type definition
  */
-type InputFieldProps = {
-  /**
-   * The input field custom className.
-   *
-   * This value can be empty. If it is, then the default className of (empty string)
-   * will be assigned. The provided className is merged with the current: "relative".
-   */
-  className?: string;
-  /**
-   * The input field default value.
-   *
-   * This value can be empty. If it is, then the default value of (empty string)
-   * will be assigned.
-   */
-  defaultValue?: string;
-  /**
-   * The input field placeholder.
-   *
-   * This value can be empty. If it is, then the default placeholder of (empty string)
-   * will be assigned.
-   */
-  placeholder?: string;
-  /**
-   * The input field children
-   *
-   * This is used to pass children to the input field component.
-   *
-   * This feature currently is not used.
-   */
-  // children?: ReactNode;
-};
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 /**
  * Eclipse Input Field Component
  *
  * @returns JSX.Element
  */
-const InputField: FC<
-  InputFieldProps & InputHTMLAttributes<HTMLInputElement>
-> = (props): JSX.Element => {
+const InputField: FC<InputProps> = (props): JSX.Element => {
   /**
-   * Set to client
+   * Set to client side rendering
    */
   "use client";
 
   /**
-   * Manage the value state.
+   * Store the value in a state
+   *
+   * This is used to keep track of the input field value.
    */
   const [value, setValue] = useState<string>(
-    props.defaultValue ?? DEFAULT_PROPS.defaultValue,
+    props.value?.toString() || props.defaultValue?.toString() || "",
   );
 
   /**
-   * When the input field is accessed and changed, this function is called.
+   * Filter the props to remove the ignored props.
    *
-   * @param e The change event
+   * This is done to prevent the ignored props from being passed to the input field.
    */
-  function onChange(e: ChangeEvent<HTMLInputElement>) {
+  const FILTERED_PROPS = Object.fromEntries(
+    Object.entries(props).filter(([key]) => !IGNORED_INPUT_PROPS.includes(key)),
+  );
+
+  /**
+   * Custom on change event handler
+   *
+   * This is used to update the value in the state.
+   */
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
+
     props.onChange?.(e);
-  }
+  };
 
   /**
    * Return the Input Field component JSX
@@ -105,10 +65,10 @@ const InputField: FC<
        */}
       <input
         className="peer w-full rounded-none border-2 border-primary bg-background p-3 font-sans font-light text-primary outline-none transition-all duration-200 ease-out placeholder:opacity-50 focus:border-primary disabled:opacity-50"
+        placeholder="" // No placeholder. We use a span for this.
         value={value}
         onChange={onChange}
-        placeholder="" // No placeholder. We use a span for this.
-        disabled={props.disabled}
+        {...FILTERED_PROPS}
       />
 
       {/**
@@ -120,8 +80,7 @@ const InputField: FC<
       <span
         className={cn(
           "font-display pointer-events-none absolute left-0 top-2 z-20 mx-2 my-2 px-2 text-sm font-light tracking-wider text-primary transition-all duration-200 ease-in-out before:absolute before:left-0 before:top-1/2 before:z-[-1] before:h-2 before:w-full before:-translate-y-[1px] before:bg-background before:transition-colors before:duration-300 before:ease-out before:content-[''] peer-focus:-top-[1.2rem] peer-focus:left-1 peer-focus:mx-2 peer-focus:px-2 peer-focus:text-sm",
-          value && // Keep the label up if there's a value
-            "left-1 -top-[1.2rem] mx-2 px-2 text-sm",
+          value && "left-1 -top-[1.2rem] mx-2 px-2 text-sm", // Keep the label up if there's a value
         )}
       >
         {props.placeholder}
--- testing/next-app/src/app/page.tsx
+++ testing/next-app/src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { InputField } from "../../../../src/index";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   return (
     <main className="flex min-h-screen w-screen h-screen flex-col items-center justify-center p-24">
-      <InputField placeholder="Test" />
+      <InputField
+        placeholder="Test"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </main>
   );
 }
