fe1d681c39f95ed2651581c9768fb5a37c61f2cf
Tristan Simpson <heytristaann@gmail.com>
2024-02-05 15:52:16 -0500

    chore: convert to client

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   EclipseLogoLong,
   RadioButton,
   Checkbox,
+  TextField,
 } from "../lib/components";
 import "./index.css";
 
@@ -16,6 +17,7 @@ export default function Home() {
 
       <MainWrapper>
         <EclipseLogoLong />
+        <TextField placeholder="Text Field" />
 
         <RadioButton placeholder="Radio Button" />
         <RadioButton placeholder="Radio Button" />
