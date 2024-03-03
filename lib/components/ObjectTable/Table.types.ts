f9643dcef829b1645cb606454c1a5e9cb733c21a
Moulik Budhiraja <moulikbudhiraja@gmail.com>
2024-03-03 18:08:39 -0500

    feat: add object table

---
+++ lib/components/ObjectTable/Table.types.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type StringKeysOnly<T> = {
+  [K in keyof T]: T[K] extends never ? never : K;
+}[keyof T] extends infer D
+  ? D extends string
+    ? D
+    : never
+  : never;
+
+export type ObjectTableProps<T extends Record<string, any>> = {
+  className?: string;
+  data: T[];
+  columns: StringKeysOnly<T>[];
+  computedColumns?: ((row: T) => ReactNode)[];
+  columnTitles?: string[];
+};
